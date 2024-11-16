1f3a475188ebe8e23cfe8e14af66ebd40606886d
AbdelhamedAbdin <medoabdin96@gmail.com>
2024-11-16 05:16:34 +0200

    add related field and time

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -12,7 +13,10 @@ class Category(models.Model):
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="prod_user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="prod_cat")
     prod_name = models.CharField(max_length=255, verbose_name="Product Name")
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.prod_name
